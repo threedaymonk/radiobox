a993602d15b23a139a72c0b45f8b997771493079
Chris Lowis <chris.lowis@gmail.com>
2011-03-26 18:36:03 +0000

    Soundcloud::API.resolve - resolve user and track title to a soundcloud ID

--- lib/soundcloud.rb
+++ lib/soundcloud.rb
@@ -1,5 +1,7 @@
 require "open-uri"
 require "json"
+require "net/http"
+require "uri"
 
 module Soundcloud
   class API
@@ -23,6 +25,18 @@ module Soundcloud
 
     def auth(url)
       "#{url}?consumer_key=#{@client_id}"
+    end
+
+    def resolve(user, track)
+      url = "#{ROOT}/resolve?url=http://soundcloud.com/#{user}/#{track}&consumer_key=#{@client_id}"
+      begin
+        response = Net::HTTP.get_response(URI.parse(url))
+        location = response.header['location']
+        track_id = URI.parse(location).path.split('/').last
+      rescue
+        return nil
+      end
+      track_id
     end
 
     def get(*u)
